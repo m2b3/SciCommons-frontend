2ff2f3f12df1fd533d466559f4556f5417c563d7
abhikrishnaram <abhikrishnaram88@gmail.com>
2024-03-12 10:21:00 +0530

    fix merge

--- src/Pages/AdminArticlePage/AdminArticlePage.jsx
+++ src/Pages/AdminArticlePage/AdminArticlePage.jsx
@@ -8,7 +8,7 @@ import { useGlobalContext } from "../../Context/StateContext";
 
 const AcceptModal = ({ setShowAccept, article, community, handleModified }) => {
   const [loading, setLoading] = useState(false);
-  const { token, user } = useGlobalContext();
+  const {token, user} = useGlobalContext();
 
   const handleAccept = async () => {
     setLoading(true);
@@ -20,9 +20,9 @@ const AcceptModal = ({ setShowAccept, article, community, handleModified }) => {
         },
       };
       const res = await axios.post(
-        `/api/article/${article}/approve_for_review/`,
-        { community: community },
-        config
+          `/api/article/${article}/approve_for_review/`,
+          {community: community},
+          config
       );
       await handleModified(article);
       if (res.status === 200) {
@@ -37,22 +37,27 @@ const AcceptModal = ({ setShowAccept, article, community, handleModified }) => {
     } catch (error) {
       console.log(error);
     }
+  }
 
-    return (
-        <>
-            <div className="w-full h-full fixed block top-0 left-0 bg-gray-900 bg-opacity-50 z-50">
-                <div className="w-full h-full flex flex-col items-center justify-center">
-                    <div className="w-5/6 md:w-1/2 bg-white p-5 rounded-lg flex flex-col items-center justify-center">
-                        <h1 className="text-md md:text-xl font-bold text-gray-600 mb-4">Are you sure you want to accept this paper for reviewal process?</h1>
-                        <div className="w-full flex flex-row items-center justify-center">
-                            <button className="text-sm font-semibold text-white p-2 px-5 mr-5 rounded-lg bg-green-600 flex" style={{cursor:"pointer"}} onClick={handleAccept}>{loading?"loading...":"Yes"}</button>
-                            <button className="text-sm font-semibold text-white p-2 px-5 rounded-lg bg-red-600 flex ml-2" style={{cursor:"pointer"}} onClick={() => {setShowAccept(false)}}>No</button>
-                        </div>
-                    </div>
-                </div>
+  return (
+      <>
+        <div className="w-full h-full fixed block top-0 left-0 bg-gray-900 bg-opacity-50 z-50">
+          <div className="w-full h-full flex flex-col items-center justify-center">
+            <div className="w-5/6 md:w-1/2 bg-white p-5 rounded-lg flex flex-col items-center justify-center">
+              <h1 className="text-md md:text-xl font-bold text-gray-600 mb-4">Are you sure you want to accept this
+                paper for reviewal process?</h1>
+              <div className="w-full flex flex-row items-center justify-center">
+                <button className="text-sm font-semibold text-white p-2 px-5 mr-5 rounded-lg bg-green-600 flex"
+                        style={{cursor: "pointer"}} onClick={handleAccept}>{loading ? "loading..." : "Yes"}</button>
+                <button className="text-sm font-semibold text-white p-2 px-5 rounded-lg bg-red-600 flex ml-2"
+                        style={{cursor: "pointer"}} onClick={() => {setShowAccept(false)}}>No
+                </button>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </>
+  )
 }
 
 const RejectModal = ({setShowReject,article,community, handleReject}) => {
@@ -264,6 +269,7 @@ const PublishModal = ({ setShowPublish, article, community }) => {
 };
 
 const AdminArticlePage = ({ community }) => {
+
   const [searchTerm, setSearchTerm] = useState("");
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -536,5 +542,4 @@ const AdminArticlePage = ({ community }) => {
     </>
   );
 };
-
 export default AdminArticlePage;
