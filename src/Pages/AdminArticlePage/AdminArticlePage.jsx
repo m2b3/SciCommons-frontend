b9bbfba7c9ed3568ab6d7e1b7cb09d7af7036fc6
abhikrishnaram <abhikrishnaram88@gmail.com>
2024-03-12 09:44:10 +0530

    re-merge ENV based backend URL

--- src/Pages/AdminArticlePage/AdminArticlePage.jsx
+++ src/Pages/AdminArticlePage/AdminArticlePage.jsx
@@ -37,46 +37,28 @@ const AcceptModal = ({ setShowAccept, article, community, handleModified }) => {
     } catch (error) {
       console.log(error);
     }
-    setLoading(false);
-    setShowAccept(false);
-  };
 
-  return (
-    <>
-      <div className="w-full h-full fixed block top-0 left-0 bg-gray-900 bg-opacity-50 z-50">
-        <div className="w-full h-full flex flex-col items-center justify-center">
-          <div className="w-5/6 md:w-1/2 bg-white p-5 rounded-lg flex flex-col items-center justify-center">
-            <h1 className="text-md md:text-xl font-bold text-gray-600 mb-4">
-              Are you sure you want to accept this paper for reviewal process?
-            </h1>
-            <div className="w-full flex flex-row items-center justify-center">
-              <button
-                className="text-sm font-semibold text-white p-2 px-5 mr-5 rounded-lg bg-green-600 flex"
-                style={{ cursor: "pointer" }}
-                onClick={handleAccept}
-              >
-                {loading ? "loading..." : "Yes"}
-              </button>
-              <button
-                className="text-sm font-semibold text-white p-2 px-5 rounded-lg bg-red-600 flex ml-2"
-                style={{ cursor: "pointer" }}
-                onClick={() => {
-                  setShowAccept(false);
-                }}
-              >
-                No
-              </button>
+    return (
+        <>
+            <div className="w-full h-full fixed block top-0 left-0 bg-gray-900 bg-opacity-50 z-50">
+                <div className="w-full h-full flex flex-col items-center justify-center">
+                    <div className="w-5/6 md:w-1/2 bg-white p-5 rounded-lg flex flex-col items-center justify-center">
+                        <h1 className="text-md md:text-xl font-bold text-gray-600 mb-4">Are you sure you want to accept this paper for reviewal process?</h1>
+                        <div className="w-full flex flex-row items-center justify-center">
+                            <button className="text-sm font-semibold text-white p-2 px-5 mr-5 rounded-lg bg-green-600 flex" style={{cursor:"pointer"}} onClick={handleAccept}>{loading?"loading...":"Yes"}</button>
+                            <button className="text-sm font-semibold text-white p-2 px-5 rounded-lg bg-red-600 flex ml-2" style={{cursor:"pointer"}} onClick={() => {setShowAccept(false)}}>No</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+        </>
+    )
+}
 
-const RejectModal = ({ setShowReject, article, community, handleReject }) => {
-  const [loading, setLoading] = useState(false);
-  const { token } = useGlobalContext();
+const RejectModal = ({setShowReject,article,community, handleReject}) => {
+
+    const [loading, setLoading] = useState(false);
+    const {token} = useGlobalContext();
 
   const handleDelete = async () => {
     setLoading(true);
