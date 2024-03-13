9f110d8669415f8071b68d8f6cfe24f498db319d
FIRSTNAME LASTNAME <rabugude@cisco.com>
2024-03-13 18:21:10 +0530

    smallchange

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import { AppProvider } from "./Context/StateContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
       <Router>
+        <NavBar
         <App />
       </Router>
     </AppProvider>
