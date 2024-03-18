9ca5027d781a49247a3dd5a994d67537012d94df
FIRSTNAME LASTNAME <rabugude@cisco.com>
2024-03-18 22:55:02 +0530

    don't include this commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import "./index.css";
 import App from "./App";
 import { AppProvider } from "./Context/StateContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./Components/NavBar/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
       <Router>
-        <NavBar
         <App />
       </Router>
     </AppProvider>
