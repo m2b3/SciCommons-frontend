b875a19dc609433e48ecba51a1687bc1a977abce
dinakar17 <85105044+dinakar17@users.noreply.github.com>
2024-06-25 00:31:13 +0530

    add type check to lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "*/**/*.{js,jsx,ts,tsx}": ["prettier --write", "eslint --fix", "eslint",  () => "tsc --skipLibCheck --noEmit"],
+  "*/**/*.{json,css,md}": ["prettier --write"]
+}
