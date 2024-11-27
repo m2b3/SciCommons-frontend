71995b0a098db26a74b2435fd117821686e9cad3
Armaan Alam <39340406+armanalam03@users.noreply.github.com>
2024-11-27 17:12:49 +0530

    Update: Banner text (release)

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -5,8 +5,7 @@ const Banner = () => {
     <div className="flex w-full flex-row items-center justify-center bg-functional-yellow/15 p-4 text-center text-yellow-700 dark:text-yellow-500">
       <div className="flex flex-row items-center space-x-4">
         <p className="text-xs md:text-sm">
-          This website is purely for testing at the moment. Stay tuned for a proper release in
-          September 2024. Thank you.
+          This website is purely for testing at the moment. Stay tuned for an alpha-release in December 2024. Thank you.
         </p>
       </div>
     </div>
