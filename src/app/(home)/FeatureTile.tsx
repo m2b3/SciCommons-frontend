304bad3819ef2d2108a246633b327fa62298498c
Armaan Alam <armanalam032001@gmail.com>
2025-01-02 21:54:07 +0530

    Enhancement: landing page, logo & footer

--- src/app/(home)/FeatureTile.tsx
+++ src/app/(home)/FeatureTile.tsx
@@ -10,8 +10,8 @@ interface FeatureTileProps {
 
 const FeatureTile: React.FC<FeatureTileProps> = ({ item }) => {
   return (
-    <div className="flex w-52 flex-col gap-2 rounded-common-xl border border-common-contrast bg-common-background p-6 md:w-96">
-      <div className="flex flex-col gap-2 md:flex-row md:items-center">
+    <div className="flex flex-col gap-4 rounded-common-xl border border-common-contrast bg-common-background p-6">
+      <div className="flex flex-col gap-4 md:flex-row md:items-center">
         <div className="rounded-full">{item?.icon}</div>
         <h3 className="text-xl font-bold text-text-primary">{item?.heading}</h3>
       </div>
