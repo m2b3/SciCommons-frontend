99a0502bb1cd117187dbc2fb03221d42fe00db25
shreyamali01 <shreya.mali@iitgn.ac.in>
2025-04-08 01:51:57 +0530

    enhanced ui of the contribution page

--- src/app/(main)/(users)/mycontributions/ContributionCard.tsx
+++ src/app/(main)/(users)/mycontributions/ContributionCard.tsx
@@ -13,16 +13,16 @@ const ContributionCard: React.FC<ContributionCardProps> = ({
   count,
   description,
 }) => (
-  <div className="flex items-start space-x-3 rounded-lg bg-white-primary p-4 shadow-md">
+  <div className="flex items-start space-x-3 rounded-lg border border-common-contrast bg-common-cardBackground p-4 res-text-xs hover:shadow-md hover:shadow-common-minimal">
     <div className="rounded-full bg-blue-100 p-2">
       <div className="h-5 w-5 text-blue-500">
         <Icon />
       </div>
     </div>
     <div>
-      <h3 className="text-base font-semibold text-gray-800">{title}</h3>
+      <h3 className="text-base font-semibold text-text-primary">{title}</h3>
       <p className="mt-1 text-2xl font-bold text-blue-600">{count}</p>
-      <p className="mt-1 text-xs text-gray-600">{description}</p>
+      <p className="mt-1 text-xs text-text-secondary">{description}</p>
     </div>
   </div>
 );
@@ -30,7 +30,7 @@ const ContributionCard: React.FC<ContributionCardProps> = ({
 export default ContributionCard;
 
 export const ContributionCardSkeleton: React.FC = () => (
-  <div className="flex animate-pulse items-start space-x-3 rounded-lg bg-white-primary p-4 shadow-md">
+  <div className="flex animate-pulse items-start space-x-3 rounded-lg border border-common-contrast bg-common-cardBackground p-4 res-text-xs hover:shadow-md hover:shadow-common-minimal">
     <div className="rounded-full bg-blue-100 p-2">
       <div className="h-5 w-5 rounded-full bg-gray-300"></div>
     </div>
