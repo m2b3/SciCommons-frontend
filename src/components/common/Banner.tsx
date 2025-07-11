d9abef3092617a52dc499395943c8cb58907df76
airfusion64 <fa058592@gmail.com>
2025-07-11 21:48:36 +0530

    updated the message on the website

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -5,8 +5,8 @@ const Banner = () => {
     <div className="flex w-full flex-row items-center justify-center rounded-t-3xl bg-functional-yellow/15 p-4 pb-16 text-center text-yellow-700 dark:text-yellow-500">
       <div className="flex flex-row items-center space-x-4">
         <p className="text-xs md:text-sm">
-          This website is purely for testing at the moment. Stay tuned for an alpha-release in March
-          2025. Thank you.
+          This website is purely for testing at the moment. Stay tuned for an alpha-release in
+          September 2025. Thank you.
         </p>
       </div>
     </div>
