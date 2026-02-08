3915d67dc5fcd8b4f5c228474e65172e74770fd0
bsureshkrishna <98675732+bsureshkrishna@users.noreply.github.com>
2026-02-08 01:27:24 -0500

    banner update

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 const Banner = () => {
   return (
     <div className="flex w-full flex-row items-center justify-center rounded-t-3xl bg-functional-yellow/15 p-4 pb-16 text-center text-yellow-700 dark:text-yellow-500">
-      <div className="flex flex-row items-center space-x-4">
-        <p className="text-xs md:text-sm">
-          This website is purely for testing at the moment. Stay tuned for an alpha-release in
-          September 2025. Thank you.
-        </p>
+      <div className="flex flex-col items-start space-y-2">
+        <ul className="list-disc space-y-1 pl-5 text-xs md:text-sm">
+          <li>
+            We are getting closer to release. Set up a community and use it for discussions like
+            Neurostars or Discourse.
+          </li>
+          <li>Set up a journal club for your group: it works !</li>
+        </ul>
       </div>
     </div>
   );
