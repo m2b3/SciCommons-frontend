68472470f0679d581c4c7e04d038a1e9e2298bf7
Abhishek2634 <abhishekfarshwal2634@gmail.com>
2026-02-14 19:37:49 +0530

    fix: bugs

--- next.config.mjs
+++ next.config.mjs
@@ -53,6 +53,7 @@ const securityHeaders = [
 const nextConfig = withPWA({
   // Enable React Strict Mode for better development experience
   reactStrictMode: true,
+  trailingSlash: true, 
 
   // Remove X-Powered-By header for security (hides that you're using Next.js)
   poweredByHeader: false,
@@ -60,7 +61,7 @@ const nextConfig = withPWA({
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*', // 
+        destination: 'http://127.0.0.1:8000/api/:path*',
       },
     ]
   },
