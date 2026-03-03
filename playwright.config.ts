51158676f752a0dfca98e53b261bfee6c7c69c16
Abhishek2634 <abhishekfarshwal2634@gmail.com>
2026-03-03 13:32:24 +0530

    adding multiple browsers for testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,9 +13,44 @@ export default defineConfig({
   },
 
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+  {
+    name: 'chromium',
+    use: { ...devices['Desktop Chrome'] },
+  },
+  {
+    name: 'firefox',
+    use: { ...devices['Desktop Firefox'] },
+  },
+  {
+    name: 'webkit',
+    use: { ...devices['Desktop Safari'] },
+  },
+  {
+    name: 'Mobile Chrome',
+    use: { ...devices['Pixel 5'] },
+  },
+  {
+    name: 'Mobile Safari',
+    use: { ...devices['iPhone 12'] },
+  },
+  {
+      name: 'Small Phone (iPhone SE)',
+      use: { ...devices['iPhone SE'] }, 
     },
-  ],
+    {
+      name: 'Tablet Landscape',
+      use: { ...devices['iPad Pro 11 landscape'] },
+    },
+    {
+      name: 'Galaxy Fold',
+      use: { ...devices['Galaxy Z Fold 5'] },
+    },
+    {
+      name: 'Wide Desktop',
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport: { width: 2560, height: 1440 },
+      },
+    },
+],
 });
