29d15c20f295fa78e306408db8b99d286533bcb6
Abhishek2634 <abhishekfarshwal2634@gmail.com>
2026-03-13 08:36:41 +0530

    Fix: UI Review Findings

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -7,7 +7,7 @@ const Loader = () => {
     <div className="md::h-[90vh] flex h-[80vh] flex-col items-center justify-center bg-transparent p-6">
       {/* {loaders[type]} */}
       <div className="w-24 md:w-32">
-        <InfiniteSpinnerAnimation color="#16A34A" strokeWidth={12} />
+        <InfiniteSpinnerAnimation color="#0C5E58" strokeWidth={12} />
       </div>
       <div
         className={`mt-4 animate-pulse text-center text-sm font-semibold text-functional-greenContrast`}
